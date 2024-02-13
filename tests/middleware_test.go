d6329991984b1ad9ee944b44051ba83f7d015231
Théotime Rivière <t@ffs.holdings>
2024-02-13 17:56:05 +0100

    add middleware test

--- tests/middleware_test.go
+++ tests/middleware_test.go
@@ -1,56 +1,67 @@
 package tests
 
-// import (
-// 	"context"
-// 	"testing"
+import (
+	"context"
+	"testing"
 
-// 	"github.com/reversTeam/go-ms/core"
-// 	"github.com/stretchr/testify/assert"
-// 	"google.golang.org/grpc"
-// )
+	"github.com/reversTeam/go-ms/core"
+	"google.golang.org/grpc"
+)
 
-// type mockMiddleware struct {
-// 	core.BaseMiddleware
-// }
+type mockMiddleware struct {
+	core.Middleware
+}
 
-// func (m *mockMiddleware) Apply(ctx context.Context, req interface{}) (context.Context, interface{}, error) {
-// 	// Mock apply function logic here
-// 	return ctx, req, nil
-// }
+func (m *mockMiddleware) Apply(ctx context.Context, req interface{}) (context.Context, interface{}, error) {
+	return ctx, req, nil
+}
 
-// func TestBaseMiddlewareUnary(t *testing.T) {
-// 	middleware := &mockMiddleware{}
-// 	interceptor := middleware.Unary()
+func TestBaseMiddlewareUnary(t *testing.T) {
+	middleware := core.BaseMiddleware{Middleware: &mockMiddleware{}}
+	interceptor := middleware.Unary()
 
-// 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-// 		return req, nil
-// 	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return req, nil
+	}
 
-// 	resp, err := interceptor(context.Background(), "request", &grpc.UnaryServerInfo{}, handler)
+	_, err := interceptor(context.Background(), "request", &grpc.UnaryServerInfo{}, handler)
+	if err != nil {
+		t.Errorf("Interceptor returned an error: %v", err)
+	}
+}
 
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, "request", resp)
-// }
+func TestBaseMiddlewareStream(t *testing.T) {
+	middleware := core.BaseMiddleware{Middleware: &mockMiddleware{}}
+	interceptor := middleware.Stream()
 
-// func TestBaseMiddlewareStream(t *testing.T) {
-// 	middleware := &mockMiddleware{}
-// 	interceptor := middleware.Stream()
+	handler := func(srv interface{}, stream grpc.ServerStream) error {
+		return nil
+	}
 
-// 	handler := func(srv interface{}, stream grpc.ServerStream) error {
-// 		return nil
-// 	}
+	mockStream := &mockServerStream{}
 
-// 	mockStream := &mockServerStream{}
+	err := interceptor(nil, mockStream, &grpc.StreamServerInfo{}, handler)
+	if err != nil {
+		t.Errorf("Interceptor returned an error: %v", err)
+	}
+}
 
-// 	err := interceptor(nil, mockStream, &grpc.StreamServerInfo{}, handler)
+type mockServerStream struct {
+	grpc.ServerStream
+	ctx context.Context
+}
 
-// 	assert.Nil(t, err)
-// }
+func (m *mockServerStream) Context() context.Context {
+	if m.ctx != nil {
+		return m.ctx
+	}
+	return context.Background()
+}
 
-// type mockServerStream struct {
-// 	grpc.ServerStream
-// }
+func (m *mockServerStream) SendMsg(msg interface{}) error {
+	return nil
+}
 
-// func (m *mockServerStream) Context() context.Context {
-// 	return context.Background()
-// }
+func (m *mockServerStream) RecvMsg(msg interface{}) error {
+	return nil
+}
